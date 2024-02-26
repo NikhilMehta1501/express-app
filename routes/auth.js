f83816d1f7fe1d9164e82d745dfb0a6805b0cf02
Nikhil Mehta <nikhilmehta1501@gmail.com>
2024-02-27 00:24:01 +0530

    logout fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,27 @@
-import {Router} from 'express';
-import passport from 'passport';
-const router = Router()
+import { Router } from "express";
+import passport from "passport";
+const router = Router();
 
 // Auth with google
 // GET auth/google
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
 // Google auth Callback
 // GET auth/google/callback
-router.get('/google/callback',
-            passport.authenticate('google', { failureRedirect: '/' }), 
-            (req, res) => res.redirect('/post') 
-          );
+router.get("/google/callback", passport.authenticate("google", { failureRedirect: "/" }), (req, res) =>
+  res.redirect("/post")
+);
 
 // logout
 // GET auth/logout
-router.get('/logout', (req, res) => { 
-  req.logout()
-  res.redirect('/') 
+router.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+  // res.redirect("/");
 });
 
-export default router
+export default router;
